b158fef9857eaa19a163ec2cc8de230ee74d477f
Mazhar-hussain-Khan <87265694+Mazhar-hussain-Khan@users.noreply.github.com>
2021-08-03 11:44:06 +0500

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,15 @@
+/* Name: Mazhar Hussain   Registration No:20MDELE098
+   Email:2oMDELE098@uetmardan.edu.pk  */
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int length=0, breadth=0;
+int area=0;
+printf("Enter the length and breadth of a rectangle:\n");
+scanf("%d %d", &length,&breadth);
+area=length * breadth;
+printf("Area of the Rectangle : %d", area);
+return 0;
+}
+
